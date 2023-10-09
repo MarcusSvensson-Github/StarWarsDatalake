4dbc294872210efe3d85cd40e68d775184499eb3
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2023-10-09 15:43:25 +0200

    compose file done and datatable for df created

--- dockerfile
+++ dockerfile
@@ -1,7 +1,12 @@
 FROM python:3.12
 
+RUN pip install pandas
+
+RUN pip install requests
+
 WORKDIR /app
 
 COPY . /app
 
 CMD python3 ingestion.py
+
--- ingestion.py
+++ ingestion.py
@@ -1,13 +1,13 @@
-"""
+
 import requests
 import json
+import pandas as pd
 
-"""
 DATABASE = "starwarsdriven"
 USER = 'postgres'
 PASSWORD = 'maytheforcebewithyou' #flytta till env variabel
 PORT = '5432'
-"""
+
 
 response = requests.get('https://swapi.dev/api/people')
 
@@ -16,31 +16,31 @@ if response.status_code == 200:
     people_results = people["results"] #results hold people in API
 
     
-    
+    data = {
+        'name': [],
+        'height': [],
+        "hair_color": [],
+        "skin_color": [],
+        "eye_color": [],
+        "birth_year": [],
+        "gender": []
+    }
+
     #Du går bara igenom en page nu!
     for person in people_results:
-        name = person["name"]
-        height = int(person["height"])
-        hair_color = person["hair_color"]
-        skin_color = person["skin_color"]
-        eye_color = person["eye_color"]
-        birth_year = person["birth_year"]
-        gender = person["gender"]
-    
+        
+        data['name'].append(person["name"])
+        data['height'].append(int(person["height"]))
+        data['hair_color'].append(person["hair_color"])
+        data['skin_color'].append(person["skin_color"])
+        data['eye_color'].append(person["eye_color"])
+        data['birth_year'].append(person["birth_year"])
+        data['gender'].append(person["gender"])
 
-
-
-
-      
-    
-  
-    print("connection closed")
-
-
+    print(data)
     
 
 else:
     print(f"Error status{response.status_code}")
-"""
 
-print('hello!!!!!!!!!!')
+
